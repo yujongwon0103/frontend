8e60d9da9ac8065cad95aa8079cba28769fea86c
YuJongWon <2e40camn@naver.com>
2025-04-14 18:23:26 +0900

    init commit

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  outputDir: '../backend/src/main/resources/static', // Build Directory
+	devServer: {
+		proxy: 'http://localhost' // Spring Boot Server
+	}
 })

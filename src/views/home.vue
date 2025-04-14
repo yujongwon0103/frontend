881d533854b000b900f308cc0665a645dc97fbee
YuJongWon <2e40camn@naver.com>
2025-04-14 23:55:14 +0900

    로그인 화면 디자인 완성

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,4 +1,8 @@
 <template>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/login">Login</router-link>
+  </nav>
   <div class="about">
     <h1>This is an about page</h1>
   </div>
--- src/views/login.vue
+++ src/views/login.vue
@@ -1,68 +1,41 @@
 <template>
-  <div class="container">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <div class="hello">
-      <h1>{{ msg }}</h1>
-      <button type="button" class="btn btn-primary">Primary</button>
-      <b-button variant="primary">Button</b-button>
-      <p>
-        For a guide and recipes on how to configure / customize this project,<br>
-        check out the
-        <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-      </p>
-      <h3>Installed CLI Plugins</h3>
-      <ul>
-        <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-        <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-        <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-        <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-        <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript" target="_blank" rel="noopener">typescript</a></li>
+  <main class="form-signin w-100 m-auto">
+    <form>
+      <img class="mb-4" src="@/assets/logo.png" alt="" width="72" height="57">
+      <div class="form-floating mt-3">
+        <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+        <label for="floatingInput">이메일</label>
+      </div>
+      <div class="form-floating">
+        <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+        <label for="floatingPassword">비밀번호</label>
+      </div>
+      <div class="form-check text-start my-3">
+        <input class="form-check-input" type="checkbox" value="remember-me" id="checkDefault">
+        <label class="form-check-label" for="checkDefault">이메일 저장</label>
+      </div>
+      <button class="btn btn-primary w-100 py-2" type="submit">로그인</button>
+      <ul class="nav col-12 col-md-auto mt-3 mb-2 justify-content-center mb-md-0">
+        <li><a href="javascript:void(0);" class="nav-link px-2 link-secondary">회원가입</a></li>
+        <li><a href="javascript:void(0);" class="nav-link px-2 link-secondary">계정 찾기</a></li>
+        <li><a href="javascript:void(0);" class="nav-link px-2 link-secondary">비밀번호 찾기</a></li>
       </ul>
-      <h3>Essential Links</h3>
-      <ul>
-        <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-        <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-        <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-        <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-        <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-      </ul>
-      <h3>Ecosystem</h3>
-      <ul>
-        <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-        <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-        <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-        <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-        <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-      </ul>
-    </div>
-  </div>
+      <button class="btn" type="button">
+        <img src="@/assets/naver_login.png">
+      </button>
+      <button class="btn" type="button">
+        <img src="@/assets/kakao_login.png">
+      </button>
+      <button class="btn" type="button">
+        <img src="@/assets/apple_login.png">
+      </button>
+      <p class="mt-5 mb-3 text-body-secondary">© Y.J.W.</p>
+    </form>
+  </main>
 </template>
-
 <script lang="ts" setup>
-import { ref, onMounted } from 'vue'
+  import { ref, onMounted } from 'vue'
 
-const msg = ref("로그인");
-
-onMounted(() => {
-  console.log(msg.value);
-})
-
+  onMounted(() => {
+  })
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>

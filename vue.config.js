16f01ff1f2fffbbe54b8d90d2f4a888bcaa7cf11
YuJongWon <2e40camn@naver.com>
2025-04-14 19:18:20 +0900

    빌드 오류 수정정

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,8 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   outputDir: '../backend/src/main/resources/static', // Build Directory
-	devServer: {
-		proxy: 'http://localhost' // Spring Boot Server
-	}
+  devServer: {
+  	proxy: 'http://localhost' // Spring Boot Server
+  },
+  lintOnSave: false
 })

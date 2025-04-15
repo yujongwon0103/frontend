02602324760443968e94c1b9addc4ef540dcdfd0
YuJongWon <2e40camn@naver.com>
2025-04-15 16:00:18 +0900

    네이버 로그인 API 연결결

--- src/views/login.vue
+++ src/views/login.vue
@@ -3,30 +3,30 @@
     <form>
       <img class="mb-4" src="@/assets/logo.png" alt="" width="72" height="57">
       <div class="form-floating mt-3">
-        <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+        <input type="email" class="form-control" id="inputEmail" placeholder="name@example.com">
         <label for="floatingInput">이메일</label>
       </div>
       <div class="form-floating">
-        <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+        <input type="password" class="form-control" id="inputPassword" placeholder="Password">
         <label for="floatingPassword">비밀번호</label>
       </div>
       <div class="form-check text-start my-3">
-        <input class="form-check-input" type="checkbox" value="remember-me" id="checkDefault">
+        <input class="form-check-input" type="checkbox" value="" id="chkSaveEmail">
         <label class="form-check-label" for="checkDefault">이메일 저장</label>
       </div>
       <button class="btn btn-primary w-100 py-2" type="submit">로그인</button>
       <ul class="nav col-12 col-md-auto mt-3 mb-2 justify-content-center mb-md-0">
-        <li><a href="javascript:void(0);" class="nav-link px-2 link-secondary">회원가입</a></li>
-        <li><a href="javascript:void(0);" class="nav-link px-2 link-secondary">계정 찾기</a></li>
-        <li><a href="javascript:void(0);" class="nav-link px-2 link-secondary">비밀번호 찾기</a></li>
+        <li><a href="javascript:void(0);" class="nav-link px-2 link-secondary" id="linkSignUp">회원가입</a></li>
+        <li><a href="javascript:void(0);" class="nav-link px-2 link-secondary" id="linkFindAccount">계정 찾기</a></li>
+        <li><a href="javascript:void(0);" class="nav-link px-2 link-secondary" id="linkFindPassword">비밀번호 찾기</a></li>
       </ul>
-      <button class="btn" type="button">
+      <button class="btn" type="button" id="btnNaverLogin" @click="onBtnNaverLoginClick">
         <img src="@/assets/naver_login.png">
       </button>
-      <button class="btn" type="button">
+      <button class="btn" type="button" id="btnKakaoLogin">
         <img src="@/assets/kakao_login.png">
       </button>
-      <button class="btn" type="button">
+      <button class="btn" type="button" id="btnAppleLogin">
         <img src="@/assets/apple_login.png">
       </button>
       <p class="mt-5 mb-3 text-body-secondary">© Y.J.W.</p>
@@ -36,6 +36,16 @@
 <script lang="ts" setup>
   import { ref, onMounted } from 'vue'
 
+  function onBtnNaverLoginClick() {
+    let param : Object = {
+      response_type: "code",
+      client_id: "Um9MhjncgTDxOgo2OnH0",
+      redirect_uri: "http://localhost:8080/login",
+      state: ""
+    }
+  }
+
   onMounted(() => {
+
   })
 </script>
